4573077256b10ebad8bf3d09fe52ce75cf8dbd5c
Charles Clow <charlesclow@gmail.com>
2019-01-02 09:54:33 -0600

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -53,6 +53,7 @@ app.get('/todos/:id', (req, res) => {
         if (!todo) {
             return res.status(404).send();
         }
+        
         res.send({todo});
     }).catch((e) => {
         return res.status(400).send();
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {ObjectId} = require('mongodb');
+
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectId(),
     text: 'First test todo'
 }, {
+    _id: new ObjectId(),
     text: 'Second test todo'
 }];
 
@@ -68,6 +72,35 @@ describe('GET ./todos', () => {
                 expect(res.body.todos.length).toBe(2);
             })
             // Don't need a function for end because we aren't doing anything asynchronus
+            .end(done);
+    });
+});
+
+describe('GET ./todos/:id', () => {
+    it('should return a todo doc if provided a valid existing id', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+    it('should return 404 and empty object if provided a valid nonexisting id', (done) => {
+        const id = new ObjectId().toHexString();
+        request(app)
+            .get(`/todos/${id}`)
+            .expect(404)
+            .expect((res) => {
+                expect(res.body.todo).toBe()
+            })
+            .end(done);
+    });
+    it('should return 404 if provided an invalid id', (done) => {
+        const id = 'someid';
+        request(app)
+            .get(`/todos/${id}`)
+            .expect(404)
             .end(done);
     });
 });
