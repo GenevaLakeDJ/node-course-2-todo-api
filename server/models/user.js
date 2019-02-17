c03d0038813a94fae150f041fc1fe2910bd52afc
Charles Clow <charlesclow@gmail.com>
2019-02-17 14:39:09 -0600

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,60 @@
 // Don't need to import mongoose.js library we created, just mongoose module.
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// Define schema
-// Can use mongoose validators / schemas
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        trim: true,
         minlength: 1,
-        trim: true
-    }
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+// Define schema
+// Can use mongoose validators / schemas
+const User = mongoose.model('User', UserSchema);
 
 // export object where User: User - shortcut it for ES6
 module.exports = {User};
